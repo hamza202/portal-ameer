a4045d43d3a7c1aa7e7af45a145e160295ed136e
devameer <ameerafch@gmail.com>
2022-10-07 15:31:01 +0300

    add edits

--- src/App.vue
+++ src/App.vue
@@ -3,34 +3,31 @@
     <metainfo>
       <template v-slot:title="{ content }">{{ content }}</template>
     </metainfo>
-    <the-header/>
+    <the-header />
     <router-view></router-view>
     <the-footer></the-footer>
   </div>
 </template>
 <script>
-import { useMeta } from 'vue-meta';
+import { useMeta } from "vue-meta";
 import TheHeader from "./components/TheHeader.vue";
 import TheFooter from "./components/TheFooter.vue";
 export default {
-  components: {TheFooter, TheHeader},
-   setup () {
-        useMeta({
-        title: `Cyber Sky || Home`
-      })
+  components: { TheFooter, TheHeader },
+  setup() {
+    useMeta({
+      title: `Cyber Sky || Home`,
+    });
   },
   data() {
-    return {
-    }
+    return {};
   },
   methods: {},
   mounted() {},
   created() {
-    console.log(import.meta.env.VITE_API_URL)
+    console.log(import.meta.env.VITE_API_URL);
   },
   computed: {},
-}
+};
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,30 +1,33 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 import "primevue/resources/primevue.min.css";
 import "primevue/resources/themes/lara-light-teal/theme.css";
 import "primeicons/primeicons.css";
-import './assets/scss/style.scss'
-import App from './App.vue'
-import store from '../store/index.js';
-import  i18n  from './plugin/i18n.js'
-import { createMetaManager} from 'vue-meta'
-const metaManager = createMetaManager()
-import {globalComponents} from "./components/GlobalComponents";
+import "./assets/scss/bootstrap/bootstrap.scss";
+
+import "./assets/scss/style.scss";
+import App from "./App.vue";
+import store from "../store/index.js";
+import i18n from "./plugin/i18n.js";
+import { createMetaManager } from "vue-meta";
+const metaManager = createMetaManager();
+import { globalComponents } from "./components/GlobalComponents";
+import VueFeather from "vue-feather";
 
 //Start vee-validate
-import {ErrorMessage, Field, Form, configure} from "vee-validate";
+import { ErrorMessage, Field, Form, configure } from "vee-validate";
 configure({
-    validateOnBlur: true,
-    validateOnChange: true,
-    validateOnInput: true,
-    validateOnModelUpdate: true,
+  validateOnBlur: true,
+  validateOnChange: true,
+  validateOnInput: true,
+  validateOnModelUpdate: true,
 });
 //End vee-validate
-
 
 const app = createApp(App);
 app.use(i18n);
 app.use(store);
 app.use(metaManager);
+app.component(VueFeather.name, VueFeather);
 
 app.component("FieldValidate", Field);
 app.component("FormValidate", Form);

6348ad6b450fa1eca1afd8da968824adff344bdb
hamza abo aitah <k8668975@gmail.com>
2022-11-06 22:21:15 +0200

    Update WishLists.vue

--- src/views/WishLists.vue
+++ src/views/WishLists.vue
@@ -1,37 +1,44 @@
 <template>
-  <!--Start My Course Price-->
-  <page-header title="Wish List"/>
+  <div>
+    <!--Start My Course Price-->
+    <page-header title="Wish List"/>
 
-  <section v-if="my_course.length" class="section-padding">
-    <div class="container-content">
-      <div
-          class="row g-3 row-cols-xxl-4 row-cols-xl-3 row-cols-md-2 row-cols-1"
-      >
-        <div v-for="(course, idx) in my_course" :key="idx" class="col">
-          <CourseCard :course="course" @remove-fave="removeFave" :splice="true"></CourseCard>
+    <section v-if="my_course.length" class="section-padding">
+      <div class="container-content">
+        <div
+            class="row g-3 row-cols-xxl-4 row-cols-xl-3 row-cols-md-2 row-cols-1"
+        >
+          <div v-for="(course, idx) in my_course" :key="idx" class="col">
+            <CourseCard :course="course" @remove-fave="removeFave" :splice="true"></CourseCard>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-  <!--Start My Course Price-->
+    </section>
+    <!--Start My Course Price-->
+    <loading-content v-if="!contentLoading" class="text-center"/>
+  </div>
+
 </template>
 
 <script>
 import ApiService from "@/services/ApiService";
 import CourseCard from "@/components/courses/CourseCard.vue";
 import pageHeader from "@/components/PageHeader.vue";
+import LoadingContent from "@/components/LoadingContent.vue";
 export default {
   name: "WishLists",
-  components: {CourseCard,pageHeader},
+  components: {CourseCard,pageHeader, LoadingContent},
   data() {
     return {
       my_course: [],
+      contentLoading: false,
     }
   },
   methods: {
     getData() {
       ApiService.get("wish-lists").then((res) => {
         this.my_course = res.data.data.courses;
+        this.contentLoading = true;
       });
     },
     removeFave(id){
